1a118629c003bbc2905dfaac053c9b2a622ed971
Jacob Ole Anderson <jacob.ole.anderson@gmail.com>
2016-10-26 01:50:14 -0400

    +ActionRoll method stub

--- Libraries/OpenLegendRPGLib/DiceRoller.cs
+++ Libraries/OpenLegendRPGLib/DiceRoller.cs
@@ -1,7 +1,14 @@
-﻿namespace OpenLegendRPGLib
+﻿using System.Collections.Generic;
+
+namespace OpenLegendRPGLib
 {
    public class DiceRoller
    {
+      public List<RolledDie> ActionRoll( DiceGroup baseDice, DiceGroup attributeDice, int advantageSum )
+      {
+         var rolledList = new List<RolledDie>();
 
+         return rolledList;
+      }
    }
 }
