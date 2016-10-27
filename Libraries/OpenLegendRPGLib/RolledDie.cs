43c9a286e63a96f6b841032a61dc54ac471b3803
Jacob Ole Anderson <jacob.ole.anderson@gmail.com>
2016-10-27 00:29:22 -0400

    Add constructor that creates a rolled die by rolling it

--- Libraries/OpenLegendRPGLib/RolledDie.cs
+++ Libraries/OpenLegendRPGLib/RolledDie.cs
@@ -2,6 +2,11 @@
 {
    public class RolledDie
    {
+      public RolledDie( Die dieToRoll )
+      {
+         rolledDie = dieToRoll;
+         rolledValue = DiceRoller.Roll( rolledDie );
+      }
       public Die rolledDie;
       public int rolledValue;
    }
